2434839bedfd4a23106162a5879cda831d88e85e
robertnjunge <mubearobert@gmail.com>
2023-07-13 16:06:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+// Code your solution in this file!
+
+const driverNames = ['Joey', 'Robert', 'Ross', 'Chandler'];
+
+const returnFirstTwoDrivers = function (driverNames) {
+    return driverNames.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (driverNames) {
+    return driverNames.slice(2);
+}
+
+const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers];
+console.log(selectingDrivers[0](driverNames));
+console.log(selectingDrivers[1](driverNames));
+
+const createFareMultiplier = (fareMultiplier) => {
+    return function (fare) {
+       return fareMultiplier * fare;
+    };
+}
+
+const fareDoubler = (fare) => { return fare * 2;
+}
+
+const fareTripler = (fare) => { return fare * 3;
+}
+
+function selectDifferentDrivers(driverNames, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(driverNames) 
+}
+
+function selectDifferentDrivers(driverNames, returnLastTwoDrivers) {
+    return returnLastTwoDrivers(driverNames) 
+}
